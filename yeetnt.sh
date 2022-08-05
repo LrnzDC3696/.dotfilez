7321cad05bd02a01adf0acf2bf3f91d7d34d4334
LrnzDC3696 <lrnzdc3696@outlook.com>
2022-08-05 16:11:03 +0800

    I dunno man seems kinda sus though

--- yeetnt.sh
+++ yeetnt.sh
@@ -3,27 +3,27 @@
 sudo pacman -S --noconfirm git
 
 # https://www.atlassian.com/git/tutorials/dotfiles
-{
-    git clone --bare https://github.com/LrnzDC3696/dotfiles $HOME/.cfg &&
+# {
+#     git clone --bare https://github.com/LrnzDC3696/dotfiles $HOME/.cfg &&
 
-    function config {
-        /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-    } &&
+#     function config {
+#         /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+#     } &&
 
-    mkdir -p .config-backup &&
-    config checkout &&
+#     mkdir -p .config-backup &&
+#     config checkout &&
 
-    if [ $? = 0 ]; then
-        echo "Checked out config.";
-    else
-        echo "Backing up pre-existing dot files."; &&
-        config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-    fi;
+#     if [ $? = 0 ]; then
+#         echo "Checked out config.";
+#     else
+#         echo "Backing up pre-existing dot files."; &&
+#         config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+#     fi;
 
-    config checkout &&
-    config config status.showUntrackedFiles no &&
-    echo "done setting up stuff"
-} || { echo "YEEEEEEETT INSTALLATION FAILED" && exit 1 }
+#     config checkout &&
+#     config config status.showUntrackedFiles no &&
+#     echo "done setting up stuff"
+# } || { echo "YEEEEEEETT INSTALLATION FAILED" && exit 1 }
 
 # run the installer script
 
