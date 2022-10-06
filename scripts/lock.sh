d949a6627baa3d2c6d542ae6505db81758954047
LrnzdDC3696 <lrnzdc3696@outlook.com>
2022-10-06 15:33:45 +0800

    removed force lock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -3,6 +3,7 @@ maim /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f ~/Pictures/Wallpapers/Active/lockscreen_icon.png ]] && convert /tmp/screen.png ~/Pictures/Wallpapers/Active/lockscreen_icon.png -gravity center -composite -matte /tmp/screen.png
 # i3lock -i /tmp/screen.png -e -f && xset s 69 69
-i3lock -i /tmp/screen.png -e -f & sleep 10 && xset dpms force off
+# i3lock -i /tmp/screen.png -e -f & sleep 10 && xset dpms off
+i3lock -i /tmp/screen.png -e -f
 # !!! tf is this?
 rm /tmp/screen.png
